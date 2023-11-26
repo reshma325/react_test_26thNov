6d3d8c418aa6667f41b3b2c3440cf2fc0b79ce32
reshma325 <138295298+reshma325@users.noreply.github.com>
2023-11-26 18:09:48 +0530

    submission

---
+++ test/Question.jsx
@@ -0,0 +1,12 @@
+const grid = [[1, 1, 1], [1, 2, 3], [1, 3, 6], [2, 3, 4]]
+const topLeft = grid[0][0]
+const bottomRight = grid[2][2]
+for (i = 0; i < grid.length; i++) {
+    for (j = 0; j < grid[i].length; j++) {
+        console.log(grid[i][j])
+        cons
+
+    }
+}
+
+// console.log(topLeft,bottomRight)
